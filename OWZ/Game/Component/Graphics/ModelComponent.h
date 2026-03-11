97931bde4a256ab1008d32532519699d3694977c
itoYusei <CA01244004@st.kawahara.ac.jp>
2026-03-11 11:29:06 +0900

    ModelComponentの追加

---
+++ OWZ/Game/Component/Graphics/ModelComponent.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+class ModelComponent : public Component
+{
+	appClass(ModelComponent);
+
+public:
+	~ModelComponent();
+
+private:
+	std::unique_ptr<ModelRender>m_model;
+	std::vector<AnimationData>m_animationData;
+	AllocatedArray<AnimationClip> m_animations;
+
+	bool m_isDraw = false;
+
+public:
+	void SetModel(const char* filePath, bool isAnimation = false);
+
+	void AddAnimation(const char* filePath, int num, bool loopFlag = true);
+
+	void Draw();
+
+	void SetDrawFlag(bool drawFlag) {
+		m_isDraw = drawFlag;
+	}
+
+	bool IsDraw()const {
+		return m_isDraw;
+	}
+};
+
